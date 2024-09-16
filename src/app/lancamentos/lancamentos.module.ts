e6f4de96681ab0166148728c6579e856473610e0
Gustavo Marinho <ghmarinho@gmail.com>
2024-09-16 20:36:49 -0300

    aula 14-10-2024

--- src/app/lancamentos/lancamentos.module.ts
+++ src/app/lancamentos/lancamentos.module.ts
@@ -5,11 +5,8 @@ import { LancamentosRoutingModule } from './lancamentos-routing.module';
 import { DespesasComponent } from './despesas/despesas.component';
 import { ReceitasComponent } from './receitas/receitas.component';
 
-
 @NgModule({
   declarations: [
-    DespesasComponent,
-    ReceitasComponent
   ],
   imports: [
     CommonModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,4 +10,9 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private router: Router){}
+
+  onEntrar(): void{
+    this.router.navigate(['dashboard']);
+  }
 }
